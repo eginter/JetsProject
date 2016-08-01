9ecabbc6245fd178a10c015b12f0c9a32ccd5f0a
Elijah Ginter <ginterelijah@gmail.com>
2016-07-31 19:32:02 -0600

    Reduced Airport tester class, added startAirport method to Airport.java

--- src/airportmanager/Airport.java
+++ src/airportmanager/Airport.java
@@ -14,6 +14,27 @@ public class Airport {
 	}
 
 	// Methods
+	public void startAirportTracker(Airport airport) {
+		
+		//Instantiate new Jet array 
+		Jet[] jets = new Jet[5];
+		
+		//Instantiate 5 Jets to populate Jet array
+		jets[0] = new Jet("E8-C JStars", 449, 2700, 244_400_000);
+		jets[1] = new Jet("SR-71 Blackbird", 2200, 2900, 34_000_000);
+		jets[2] = new Jet("U-2 Dragon Lady", 500, 6400, 320_000_000);
+		jets[3] = new Jet("MC-12 Liberty", 333, 2075, 6_000_000);
+		jets[4] = new Jet("AC-130H Spectre", 300, 2200, 132_400_000);
+		
+		//Store Jet array into Airport object
+		airport.setJets(jets);
+		
+		//Instantiate new Prompter to begin display
+		AirportPrompter prompter = new AirportPrompter(airport);
+		prompter.menu();
+	}
+	
+	
 	public Jet getLongestRange() {
 		int longestRange = 0;
 		Jet longRangeJet = null;
